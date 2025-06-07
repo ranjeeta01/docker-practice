8f69c79b4176bd169e2bcca5697a75937e4a64a2
Ranjeeta Kumari <41211457+ranjeeta01@users.noreply.github.com>
2025-06-07 19:59:28 +0530

    added code in app.py

--- simple-webapp/app.py
+++ simple-webapp/app.py
@@ -1,1 +1,20 @@
 #simple webapp in flask
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+# Sample anime quotes
+quotes = [
+    "What are you so hesitant about? It’s your dream, isn’t it? It’s right in front of you, and you’re wavering? You gotta be reckless and take whatever you can!”,
+    "Throughout heaven and earth, I alone am the honored one",
+    #use an api to get quotes
+]
+
+@app.route('/')
+def index():
+    random_quote = random.choice(quotes)
+    return render_template('index.html', quote=random_quote)
+
+if __name__ == '__main__':
+    app.run(debug=True)
